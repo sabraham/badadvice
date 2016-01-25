462702470b036aee84c8783491b095146dfe1e49
Sunil Abraham <s.abraham@gmail.com>
2016-01-25 00:16:58 -0500

    Fixed stripe color

--- js/main.js
+++ js/main.js
@@ -6,13 +6,11 @@ var randomMatisse = function () {
 
 document.addEventListener("DOMContentLoaded", function(event) {
     var squares = document.getElementsByClassName('square');
-    console.log(squares.length)
     for (var i = 0; i < squares.length; ++i) {
         console.log(i);
         var square = squares[i];
-        console.log(square)
         square.style.backgroundColor = randomMatisse()
     }
-    var stripe = document.getElementsByClassName('stripe-3')[0];
+    var stripe = document.getElementsByClassName('stripe')[0];
     stripe.style.backgroundColor = randomMatisse()
 });
